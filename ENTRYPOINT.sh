d6e12979045cd2512a6285c69d9a8bd5d9d98941
ring1012 <1223305593@qq.com>
2024-10-06 21:07:12 +0800

    use env to set token

--- ENTRYPOINT.sh
+++ ENTRYPOINT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#docker run -d --privileged --restart always registry.cn-hangzhou.aliyuncs.com/ring1012/dind-ttyd:2024-10-06 --token YOUR_TOKEN
+#docker run -d --privileged --restart always -e CF_TOKEN="YOUR_TOKEN" registry.cn-hangzhou.aliyuncs.com/ring1012/dind-ttyd:2024-10-06
 
-docker run -d --restart always --name cloudflare_tunnel  cloudflare/cloudflared:latest  tunnel --no-autoupdate run $1
+docker run -d --restart always --name cloudflare_tunnel  cloudflare/cloudflared:latest  tunnel --no-autoupdate run --token $CF_TOKEN
 
 nohup ttyd -p 2086 --check-origin=false /bin/login > /dev/null 2>&1 &
