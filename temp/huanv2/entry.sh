06fa5225d295727af44d39c316a25b0b956ad730
ring1012 <tanghuan93@foxmail.com>
2024-12-16 14:06:48 +0800

    v2

---
+++ temp/huanv2/entry.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+nginx -g "daemon off;" &
+#cloudflared service install eyJhIjoiMTFiMmE0ZTU4NDhlYmI0ZjQ0NWI2NWI1YTljZDhiMTciLCJ0IjoiNDI0N2VlOGUtMWVjMC00YTAyLWFjNWEtZmM3ZGM1NjlmMDJiIiwicyI6Ik1qVmpZVEpoWlRFdFpqYzROeTAwTkRFeExXSmlPREV0TlRReE5UaGpOR0ZoWXpNdyJ9
+#/etc/init.d/cloudflared start
+v2ray run --config /ali.json > /dev/null
